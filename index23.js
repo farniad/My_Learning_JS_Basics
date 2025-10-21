98867713b68b32b29fbdc97ffd029240814af67c
farniad <lordofhosts06@gmail.com>
2025-10-21 14:32:03 -0800

    Add files via upload
    1-Create a new string from a given string changing the position of first and last characters
    2-Swap two given Characters in the text

--- index23.js
+++ index23.js
@@ -1,22 +1,31 @@
-//Create a new string from a given string 
-// changing the position of characters(chars)
+//1-Create a new string from a given string 
+//2-Changing the position of characters(chars)
+//remove a character at the specified position of a given string & return the new string
+
+function change(){
+    let text= document.getElementById("text1").value;
+    let first_index= Number(document.getElementById("first_index").value);
+    let last_index=  Number(document.getElementById("last_index").value);
+    let remove_textpart= text.slice(0,first_index) + text.slice(last_index+1);
+
+    document.getElementById("result1").innerHTML= '<t> new text :  '+  remove_textpart ;
+}
+
 
 function swap_characters(){
-    let  Text=document.getElementById("string").value;
-    let first_char= document.getElementById("first_index").value;
-    let second_char= document.getElementById("second_index").value;
+    let  Text=document.getElementById("text2").value;
+    let first_char= document.getElementById("first_char").value;
+    let second_char= document.getElementById("second_char").value;
 
     if (!first_char || !second_char){
-        document.getElementById("result").innerHTML="enter the characters";
+        document.getElementById("result1").innerHTML="enter the characters";
         return;
     }
-
-    
     let sample_text= "sample_text";
     let text_swap= Text.replaceAll(first_char,sample_text)
         .replaceAll(second_char,first_char)
         .replaceAll(sample_text,second_char);
 
-    document.getElementById("result").innerHTML= text_swap;
+    document.getElementById("result2").innerHTML= text_swap;
     
 }
